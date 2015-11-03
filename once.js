b90e916254be7a342742f33d94a908d7b6373d8e
David Shin <davidshin444@gmail.com>
2015-11-03 01:08:18 -0800

    Forgot to removed a function that wasn't working.

--- once.js
+++ once.js
@@ -116,12 +116,4 @@ var onceMap = function (anIterable, usersFunction) {
 };
 
 
-// monteCarlo - A higher order Monte Carlo simulator function.
-var monteCarlo = function (numTrials, usersFunction, input){
-    var outcomeArr = []
-    for (i = 0; i < numTrials; i++) {
-        var value = usersFunction(input);   
-    }
-};
-
 
