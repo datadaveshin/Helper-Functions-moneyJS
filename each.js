7b2938d1fb2a3e14b64e926d530223914477c3f3
David Shin <davidshin444@gmail.com>
2016-10-02 16:47:37 -0700

    Fix typo

--- each.js
+++ each.js
@@ -14,12 +14,12 @@ function $$each(collection, callback) {
     if (typeof collection === 'string') {
         collection = collection.split("")
         for (let i = 0; i < collection.length; i++) {
-            callback(collection[i], i, collection.join(""), collection['0'], collection[collection.length-1], 0, collection.length - 1, collection.length);
+            callback(collection[i], i, collection.join(""), collection[0], collection[collection.length-1], 0, collection.length - 1, collection.length);
         }
     }
     else if (Array.isArray(collection)) {
         for (let i = 0; i < collection.length; i++) {
-            callback(collection[i], i, collection, collection['0'], collection[collection.length-1], 0, collection.length - 1, collection.length);
+            callback(collection[i], i, collection, collection[0], collection[collection.length-1], 0, collection.length - 1, collection.length);
         }
     }
     else if (typeof collection === 'object') {
