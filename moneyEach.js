c98f80a57f49235739fbeb01b075b01b8b40319c
David Shin <davidshin444@gmail.com>
2016-10-02 14:28:21 -0700

    Added start end

--- moneyEach.js
+++ moneyEach.js
@@ -2,22 +2,20 @@
 A version of each
 */
 function $$each(collection, callback) {
-    let isString = false
     if (typeof collection === 'string') {
-        isString = true
         collection = collection.split("")
-    }
-    if (Array.isArray(collection)) {
         for (let i = 0; i < collection.length; i++) {
-            if (isString === true) {
-                callback(collection[i], i, collection.join(""), 0, collection.length - 1, collection.length);
-            } else {
-                callback(collection[i], i, collection, 0, collection.length - 1, collection.length);
-            }
+            callback(collection[i], i, collection.join(""), collection[0], collection[collection.length-1], 0, collection.length - 1, collection.length);
         }
-    } else if (typeof collection === 'object') {
+    }
+    else if (Array.isArray(collection)) {
+        for (let i = 0; i < collection.length; i++) {
+            callback(collection[i], i, collection, collection[0], collection[collection.length-1], 0, collection.length - 1, collection.length);
+        }
+    }
+    else if (typeof collection === 'object') {
         for (let key in collection) {
-            callback(collection[key], key, collection, collection[Object.keys(collection)[0]], collection[(Object.keys(collection)[Object.keys(collection).length - 1])], Object.keys(collection).length);
+            callback(collection[key], key, collection, collection[Object.keys(collection)[0]], collection[(Object.keys(collection)[Object.keys(collection).length - 1])], Object.keys(collection)[0], Object.keys(collection)[Object.keys(collection).length - 1], Object.keys(collection).length);
         }
     }
 }
@@ -27,9 +25,10 @@ console.log("")
  console.log("# On Arrays")
  console.log("################################")
 
-var myArr = [1,2,3]
-$$each(myArr, function(item, index, array, start, end, length) {
-    console.log(item, index, array, start, end, length)
+var myArr = ['a', 'b', 3, 'd']
+$$each(myArr, function(item, index, array, first, last, start, end, length) {
+    console.log('item', 'index', 'array', 'first', 'last', 'start', 'end', 'length')
+    console.log(item, index, array, first, last, start, end, length)
 })
 
 console.log("")
@@ -37,9 +36,10 @@ console.log("")
  console.log("# On strings")
  console.log("################################")
 
-var myStr = "123"
-$$each(myStr, function(item, index, array, start, end, length) {
-    console.log(item, index, array, start, end, length)
+var myStr = "ab3d"
+$$each(myStr, function(item, index, array, first, last, start, end, length) {
+    console.log('item', 'index', 'array', 'first', 'last', 'start', 'end', 'length')
+    console.log(item, index, array, first, last, start, end, length)
 })
 
 console.log("")
@@ -48,7 +48,8 @@ console.log("")
  console.log("################################")
 
 
-var myObj = {'a': 'one', 'b': 'two', 'c': "three"}
-$$each(myObj, function(item, index, array, start, end, length) {
-    console.log(item, index, array, start, end, length)
+var myObj = {'aaa': 'one', 'b': 'two', 'cee': 3, 'd': "four"}
+$$each(myObj, function(item, index, array, first, last, start, end, length) {
+    console.log('item', 'index', 'array', 'first', 'last', 'start', 'end', 'length')
+    console.log(item, index, array, first, last, start, end, length)
 })
